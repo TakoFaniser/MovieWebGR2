3b9fd9581d7c91dd702cd61785ec4bed87d71dbf
Tako <quantnkd@gmail.com>
2025-04-18 01:23:24 +0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'swiper/swiper.min.css';
-import './assets/boxicons-2.0.7/css/boxicons.min.css';
+import './assets/boxicons-2.1.4/css/boxicons.min.css';
 import './App.scss';
 
 import { BrowserRouter, Route } from 'react-router-dom';
