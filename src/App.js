a49d7c2504251bc04b2c822bec14fddb1a4200a9
Tako <162946492+TakoFaniser@users.noreply.github.com>
2025-04-27 17:36:42 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Routes from './config/Routes';
 function App() {
     return (
         <BrowserRouter>
-        <BrowserRouter basename="/MovieWebGR2">
+        
             <Route render={props => (
                 <>
                     <Header {...props}/>
@@ -20,7 +20,7 @@ function App() {
                     <Footer/>
                 </>
             )}/>
-        </BrowserRouter>
+        
         </BrowserRouter>
     );
 }
